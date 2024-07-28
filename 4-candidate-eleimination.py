d2dda6f75b33fe162896b9180905f87da5d7112a
Rakshitha-Pai <121934659+Rakshitha-Pai@users.noreply.github.com>
2024-07-28 23:19:54 +0530

    4.candidate-eleimination.py

--- 4-candidate-eleimination.py
+++ 4-candidate-eleimination.py
@@ -30,3 +30,47 @@ s_final, g_final = learn(concepts, target)
 
 print(f"Final S: {s_final}")
 print(f"Final G: {g_final}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import pandas as pd
+
+df=pd.read_csv('enjoysport.csv')
+data = list(df.values)
+
+specific = data[0][:-1]
+general = [['?' for i in range(len(specific))] for j in range(len(specific))]
+
+for i in data:
+    if i[-1] == "yes":
+        for j in range(len(specific)):
+            if i[j] != specific[j]:
+                specific[j] = "?"
+                general[j][j] = "?"
+
+    elif i[-1] == "no":
+        for j in range(len(specific)):
+            if i[j] != specific[j]:
+                general[j][j] = specific[j]
+            else:
+                general[j][j] = "?"
+  
+gh = [] 
+for i in general:
+    for j in i:
+        if j != '?':
+            gh.append(i)
+            break
+print("\nFinal Specific hypothesis:\n", specific)
+print("\nFinal General hypothesis:\n", gh)
